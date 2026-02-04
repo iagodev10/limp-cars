f029fbf0314fbd94378e3bd265a112e6872c9267
Iago Borges <iagoborgesbarbosa7@gmail.com>
2026-02-04 08:23:27 -0300

    Replace CTAButton with CTAButtonServicos

--- limpcars-frontend/src/Pages/Home/index.jsx
+++ limpcars-frontend/src/Pages/Home/index.jsx
@@ -39,7 +39,7 @@ const Home = () => {
                             Seu veículo merece o melhor tratamento.
                         </HeroDescription>
                         <CTAWrapper>
-                            <CTAButton as={Link} to="/servicos">Agendar Serviço</CTAButton>
+                            
                             <CTAButtonServicos as={Link} to="/servicos">Ver Serviços</CTAButtonServicos>
                         </CTAWrapper>
                     </Conteudo>
